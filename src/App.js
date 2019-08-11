bfe557360eef8ad685bd6a76ae2d780914041feb
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-11 02:50:44 +0100

    WIP: setup User component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ReactDom from 'react-dom'
 import './App.css';
+import User from './components/User'
 
 function App() {
+  const [user, setUser] = useState({})
+  
+  useEffect(() => {
+    setUser({ firstName: 'Chris', lastName: 'Jones' })
+  }, [])
+
   return (
     <div className="App">
-      <p>placeholder app</p>
+      <User />
     </div>
   );
 }
